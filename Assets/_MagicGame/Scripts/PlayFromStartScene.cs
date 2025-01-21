ed42087706f28cc2853ee972a251df2045f323cd
Jalagon <A1ag0nJus1in@gmail.com>
2025-01-21 09:53:18 -0800

    Spellbook mechanic started

--- Assets/_MagicGame/Scripts/PlayFromStartScene.cs
+++ Assets/_MagicGame/Scripts/PlayFromStartScene.cs
@@ -2,36 +2,36 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// [InitializeOnLoad]
+[InitializeOnLoad]
 public static class PlayFromStartScene
 {
-    // static PlayFromStartScene()
-    // {
-    //     EditorApplication.playModeStateChanged += LoadStartScene;
-    // }
+    static PlayFromStartScene()
+    {
+        EditorApplication.playModeStateChanged += LoadStartScene;
+    }
 
-    // private static void LoadStartScene(PlayModeStateChange state)
-    // {
-    //     // Check if entering Play Mode
-    //     if (state == PlayModeStateChange.EnteredPlayMode)
-    //     {
-    //         // Define your desired starting scene
-    //         string startSceneName = "MainMenuScene";
+    private static void LoadStartScene(PlayModeStateChange state)
+    {
+        // Check if entering Play Mode
+        if (state == PlayModeStateChange.EnteredPlayMode)
+        {
+            // Define your desired starting scene
+            string startSceneName = "MainMenuScene";
 
-    //         // Check if the active scene is not the desired start scene
-    //         if (SceneManager.GetActiveScene().name != startSceneName)
-    //         {
-    //             // Load the starting scene
-    //             Scene startScene = SceneManager.GetSceneByName(startSceneName);
-    //             if (startScene != null)
-    //             {
-    //                 SceneManager.LoadScene(startSceneName);
-    //             }
-    //             else
-    //             {
-    //                 Debug.LogError($"Scene '{startSceneName}' not found! Make sure it's in the build settings.");
-    //             }
-    //         }
-    //     }
-    // }
+            // Check if the active scene is not the desired start scene
+            if (SceneManager.GetActiveScene().name != startSceneName)
+            {
+                // Load the starting scene
+                Scene startScene = SceneManager.GetSceneByName(startSceneName);
+                if (startScene != null)
+                {
+                    SceneManager.LoadScene(startSceneName);
+                }
+                else
+                {
+                    Debug.LogError($"Scene '{startSceneName}' not found! Make sure it's in the build settings.");
+                }
+            }
+        }
+    }
 }
