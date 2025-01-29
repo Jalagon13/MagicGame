c0583c18b5e53f847c928baf28697397913397b2
Jalagon <A1ag0nJus1in@gmail.com>
2025-01-29 15:50:01 -0800

    Behavior graph installed

--- Assets/_MagicGame/Scripts/ScriptableObjects/NpcDataBaseSO.cs
+++ Assets/_MagicGame/Scripts/ScriptableObjects/NpcDataBaseSO.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu()]
+// [CreateAssetMenu()]
 public class NpcDataBaseSO : ScriptableObject
 {
     public List<NpcSO> NpcSOList;
