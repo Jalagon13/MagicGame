6f0696ae9508b8854c6e128d4f6104f7318469a5
Jalagon <A1ag0nJus1in@gmail.com>
2025-03-28 08:14:10 -0700

    Sprite opacity bug fixed

--- Assets/_MagicGame/Scripts/Managers/SoundManager.cs
+++ Assets/_MagicGame/Scripts/Managers/SoundManager.cs
@@ -5,17 +5,19 @@ using FMOD.Studio;
 public class SoundManager : MonoBehaviour
 {
 	public static SoundManager Instance { get; private set; }
-	
+
 	private void Awake()
 	{
 		Instance = this;
 	}
-	
+
+	// Play a sound one time at a specific world position
 	public void PlayOneShot(EventReference sound, Vector3 worldPos)
 	{
 		RuntimeManager.PlayOneShot(sound, worldPos);
 	}
-	
+
+	// Create an event instance
 	public EventInstance CreateInstance(EventReference eventReference)
 	{
 		EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
