4306c8152c0ff314846fc5ebbe2de5d4c63a9844
Jalagon <A1ag0nJus1in@gmail.com>
2025-05-02 16:28:26 -0700

    More generation edits

--- Assets/_MagicGame/Scripts/_ProceduralGeneration/ExpandShoreLineGenerationStep.cs
+++ Assets/_MagicGame/Scripts/_ProceduralGeneration/ExpandShoreLineGenerationStep.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 
 public class ExpandShoreLineGenerationStep : GenerationStep
 {
+    [field: SerializeField] public NoiseMapSO SandNoiseMap { get; private set; }
     [field: SerializeField] public int Width { get; private set; }
+
+    [field: Header("Terrain Threshold Values")]
+    [field: SerializeField, Range(0, 1)]
+    public float SandThreshold { get; private set; } = 0.5f;
 
     [field: Header("Terrain Tiles")]
     [field: SerializeField] public TileSO SandTile { get; private set; }
@@ -20,10 +25,17 @@ public class ExpandShoreLineGenerationStep : GenerationStep
             // Add more here in the future
         };
 
-        HashSet<Vector2Int> initialShoreTiles = GenerationUtils.GetEdgeTiles(_tilesToSearchFor, new List<TileSO>() { WaterTile }, DirectionsHelper.DirectionOffsets8, genData.TileMatrix);
-        foreach (Vector2Int shoreTile in initialShoreTiles) // Need to do it like this so tilematrix gets updated
+        HashSet<Vector2Int> initialShoreTiles = new();
+        HashSet<Vector2Int> allInitialShoreTiles = GenerationUtils.GetEdgeTiles(_tilesToSearchFor, new List<TileSO>() { WaterTile }, DirectionsHelper.DirectionOffsets8, genData.TileMatrix);
+        foreach (Vector2Int shoreTile in allInitialShoreTiles) // Need to do it like this so tilematrix gets updated
         {
-            genData.SetTileData(shoreTile.x, shoreTile.y, SandTile);
+            float noiseValue = SandNoiseMap.NoiseTexture.GetPixel(shoreTile.x, shoreTile.y).grayscale;
+
+            if (noiseValue > SandThreshold)
+            {
+                genData.SetTileData(shoreTile.x, shoreTile.y, SandTile);
+                initialShoreTiles.Add(shoreTile);
+            }
         }
         
         // Width - 1 because initial shore tiles are already expanded
